27db2d54394beab9607bb917923f94ee276d8777
Hammed Iyiola Jimoh <103585446+IyiolaJay@users.noreply.github.com>
2024-04-01 12:58:42 +0100

    Username regEx fix (#10)

--- src/api/middlewares/validators/profile.validators.js
+++ src/api/middlewares/validators/profile.validators.js
@@ -1,11 +1,17 @@
 import Joi from "joi";
 
 export const createUsernameValidator = Joi.object({
-    username: Joi.string().required(),
-  });
+  username: Joi.string()
+    .min(4)
+    .pattern(/^[a-zA-Z0-9_.]{4,}$/, 'username must include alphanumeric, underscore, and full stop characters only, no spaces allowed')
+    .required(),
+});
 
 
   export const changeUsernameValidator = Joi.object({
-    newUsername: Joi.string().required(),
+    newUsername: Joi.string()
+        .min(4)
+        .pattern(/^[a-zA-Z0-9_.]{4,}$/, 'username must include alphanumeric, underscore, and full stop characters only, no spaces allowed')
+        .required(),
     prevUsername: Joi.string().required(),
   });
