06c091b69c06651b0633aecc016b2dfa5a13721b
Hammed Iyiola Jimoh <103585446+IyiolaJay@users.noreply.github.com>
2024-04-01 07:50:28 +0100

    Staging (#7)
    * Hot fix
    
    * generate codes

--- src/database/index.js
+++ src/database/index.js
@@ -8,7 +8,7 @@ export const connectToDatabase = async () => {
 
     console.log("MongoDB connection successful!");
 
-    // await createDefaultInviteCodes() //uncomment this line to create invite codes
+    await createDefaultInviteCodes() //uncomment this line to create invite codes
   } catch (error) {
     console.log("Failed to connect to Database: ", error);
     process.exit(1);
