d29c6ee1f4b81d8456d812b8896ba5666e467f99
Hammed Iyiola Jimoh <103585446+IyiolaJay@users.noreply.github.com>
2024-04-20 14:23:49 +0100

    Staging (#20)
    * Added membership status to getAllSpace
    
    * Dev/iyiola (#17)
    
    * Added membership status to getAllSpace (#16)
    
    * total users statistics
    
    * fixed leak
    
    * Dev/iyiola (#19)
    
    * Added membership status to getAllSpace (#16)
    
    * total users statistics
    
    * fixed leak
    
    * exposed space leaderboard endpoint

--- src/analytics/services/app/space.analytics.service.js
+++ src/analytics/services/app/space.analytics.service.js
@@ -1,3 +1,4 @@
+import { Types } from "mongoose"
 import SpacePoints from "../../../database/models/spaces/spacePointsRanking.js"
 import Spaces from "../../../database/models/spaces/spaces.js"
 import { ErrResourceNotFound } from "../../../errors/index.js"
@@ -8,11 +9,37 @@ const  getSpaceLeaderShipBoardDataService = async(spaceId)=>{
 
     if(!space) throw ErrResourceNotFound;
 
-    const leaderBoard = await SpacePoints.find({
-        space_id : space._id,
-    }).populate("user_id")
+    // const leaderBoard = await SpacePoints.find({
+    //     space_id : space._id,
+    // }).populate("user_id")
 
-    return leaderBoard;
+    // return leaderBoard;
+
+    const __id = new Types.ObjectId("6609f6f0d65b46ae84cb318a")
+
+    const spacePoints = await SpacePoints.aggregate([
+        { $match: { space_id: __id } },
+        {
+          $lookup: {
+            from: "usernames",
+            localField: "user_id",
+            foreignField: "user_id",
+            as: "username_info",
+          },
+        },
+        {
+          $unwind: "$username_info",
+        },
+        {
+          $group: {
+            username: "$username_info.username",
+            totalPoints: { $sum: "$points" },
+          },
+        },
+        { $sort: { totalPoints: -1 } },
+      ]);
+  
+      return spacePoints;
 }
 
 
