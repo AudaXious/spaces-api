f125dda325f586315b3932bfef4312e675c1e717
Hammed Iyiola Jimoh <103585446+IyiolaJay@users.noreply.github.com>
2024-04-25 21:44:29 +0100

    Fetch space image on GetACampaign (#23)

--- src/api/services/app/campaign.service.js
+++ src/api/services/app/campaign.service.js
@@ -173,6 +173,7 @@ const getACampaignService = async(campaignId)=>{
         $addFields: {
           space_title: '$space.title',
           space_uuid: '$space.uuid',
+          space_id: '$space._id',
           isVerified: '$space.isVerified'
         },
       },
@@ -197,6 +198,28 @@ const getACampaignService = async(campaignId)=>{
           iconUrl :  { $arrayElemAt: ['$attachments.url', 0] },
         },
       },
+      //
+      {
+        $lookup: {
+            from: 'attachments',
+            let: { space_id: '$space_id' },
+            pipeline: [
+                {
+                    $match: {
+                        $expr: { $eq: ['$item_id', '$$space_id'] },
+                    },
+                },
+            ],
+            as: 'spaceAttachments',
+        },
+    },
+    {
+      $addFields: {
+          spaceIconUrl: { $ifNull: [{ $arrayElemAt: ['$spaceAttachments.url', 0] }, null] },
+          spaceBannerUrl: { $ifNull: [{ $arrayElemAt: ['$spaceAttachments.url', 1] }, null] },
+      },
+    },
+      //
       {
         $project: {
           title: 1,
@@ -207,6 +230,8 @@ const getACampaignService = async(campaignId)=>{
           taskCount: { $size: '$tasks' },
           space_title: 1,
           space_uuid: 1,
+          spaceIconUrl : 1,
+          spaceBannerUrl : 1,
           isVerified : 1,
           tasks: 1,
           taskParticipantCount: { $size: '$taskParticipants' }, 
